4a4c64bc696abeb0d6f33256d862226a2517794e
Ray Winkelman <rwinkelman@beyondtrust.com>
2017-03-27 16:44:59 -0300

    Demos calling virtual method in base constructor.

---
+++ c-sharp-interview/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace c_sharp_interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MostDerived o = new MostDerived();
+            o.Print("text");
+
+            Console.Read();
+        }
+    }
+
+    abstract class Base 
+    {
+        protected Base()
+        {
+            Console.WriteLine("base constructor.");
+            Print("null");
+        }
+
+        public virtual void Print(string message)
+        {
+            Console.WriteLine("base print method.");
+        }
+    }
+
+    class Medial : Base
+    {
+        public Medial()
+        {
+            Console.WriteLine("medial constructor.");
+        }
+
+        public override void Print(string message)
+        {
+            Console.WriteLine("medial print method.");
+        }
+    }
+
+    class MostDerived : Medial
+    {
+        string instanceInit = null;
+        string typeInit = " wow, I was type initialized.";
+
+        public MostDerived()
+        {
+            Console.WriteLine("most derived constructor.");
+            instanceInit = " wow, I was instance initialized.";
+        }
+
+        public override void Print(string message)
+        {
+            Console.WriteLine("most derived print method." + typeInit + instanceInit);
+        }
+
+        public void GetCaller([CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "")
+        {
+            Console.WriteLine(memberName);
+            Console.WriteLine(sourceFilePath);
+        }
+    }
+}
